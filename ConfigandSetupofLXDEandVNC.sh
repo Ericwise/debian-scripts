e5bf49231fabe503dfc2e67ddf51613134619637
6donote4 <6donote4@gmail.com>
2017-06-15 09:15:22 +0000

    Shell script

--- ConfigandSetupofLXDEandVNC.sh
+++ ConfigandSetupofLXDEandVNC.sh
@@ -1,4 +1,6 @@
+​
+#Configure desktop environment , VNC server and Chromium web browser on Debian server.
+#Debian服務器桌面環境、VNC及谷歌瀏覽器配置腳本
 #!/bin/bash
-yes | apt-get install lxde tightvncserver xfonts-base chromium
-
-exit 0
+yes | apt-get install lxde lxlauncher lxtask tightvncserver xfonts-base chromium xfce4-power-manager update-notifier usermode ttf-way-microhei matchbox-keyboard
+dpkg-reconfigure locales
