de2aa4e777529035320e39dc03b33bc6d1dffe5f
6donote4 <do_note@hotmail.com>
2018-09-09 18:19:26 +0800

    A script used to delete files in Linux

--- delfiles.sh
+++ delfiles.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# This script is used to delete selected in Linux
+# This script is used to delete selected files in Linux
 
 red='\033[0;31m'
 green='\033[0;32m'
@@ -8,23 +8,31 @@ plain='\033[0m'
 
 while true
 do
-read -p "Please input file name:" filename
-echo "filename = ${filename}"
-echo
-
-read -p "Please input file size:" filesize
-echo "filesize = ${filesize}"
-echo
-
-find . -size -${filesize} -type f -iname ${filename} -delete
-
+	echo "1)delete specified size files"
+	echo "2)delete empty directory"
+    read -p "Please select a operation:" OPERATION
+	echo "selected $OPERATION"
+	case $OPERATION in
+		1)
+			read -p "Please input file name:" filename
+			echo "filename = ${filename}"
+			echo
+            read -p "Please input file size:" filesize
+            echo "filesize = ${filesize}"
+            echo
+            find . -size -${filesize} -type f -iname ${filename} -delete
+	        echo "done"
+			;;
+		2)  
+			find . -empty -delete
+			echo "done"
+			;;
+	esac
 read -p "Does continue ?(y,n)" confirm
 echo "confirm = ${confirm}"
 echo
 if [ $confirm = "n" ];then
 	break
 fi
-
 done
-
 exit 0 
