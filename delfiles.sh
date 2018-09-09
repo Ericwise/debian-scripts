a054c15c1d421dbd6abb072f15e484b8dbc0c9f8
6donote4 <do_note@hotmail.com>
2018-09-09 19:27:50 +0800

    A shell script to update Linux system

--- delfiles.sh
+++ delfiles.sh
@@ -11,28 +11,29 @@ do
 	echo "1)delete specified size files"
 	echo "2)delete empty directory"
     read -p "Please select a operation:" OPERATION
-	echo "selected $OPERATION"
+	echo "selected $OPERATION "
 	case $OPERATION in
 		1)
-			read -p "Please input file name:" filename
-			echo "filename = $FILENAME"
+			read -p "Please input file name:" FILENAME
+			echo "filename = $FILENAME "
 			echo
-            read -p "Please input file size:" filesize
-            echo "filesize = $FILESIZE"
-            echo
-            find . -size -$FILESIZE -type f -iname $FILENAME -delete
-	        echo "done"
+                        read -p "Please input file size:" FILESIZE
+                        echo "filesize = $FILESIZE "
+                        echo
+                        find . -size -$FILESIZE -type f -iname $FILENAME -delete
+	                echo "done"
 			;;
 		2)  
 			find . -empty -delete
 			echo "done"
 			;;
 	esac
-read -p "Does continue ?(y,n)" RESPONSE
-echo "confirm = $RESPONSE"
-echo
-if [ $RESPONSE = "n" ];then
+        read -p "Does continue ?(y,n)" RESPONSE
+        echo "confirm = $RESPONSE "
+        echo
+        if [ $RESPONSE = "n" ];then
 	break
-fi
-echo "done"
-exit 0 
+        fi
+        echo "done"
+done
+exit 0
---
+++ updatesystem.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#This shell script is used to update and upgrade Linux release system
+
+while true
+do
+	echo "1)Debian/GNU APT"
+	echo "2)ArchLinux/GNU PACMAN"
+	read -p "Please select a release:" NUM
+	echo "Selected $NUM)"
+	case $NUM in
+		1)
+			proxychains apt-get update -y ;
+			proxychains apt-get upgrade -y ;
+			echo "done"
+			;;
+		2)
+	                proxychains pacman -Suu -y;
+	                echo "done"
+			;;
+		esac
+    read -p "Does continue?:(y,n) " RESPONSE
+            echo "$RESPONSE"
+    if [ $RESPONSE == "n" ]; then
+    	break
+    fi
+echo "done"
+done
+exit 0
+			
